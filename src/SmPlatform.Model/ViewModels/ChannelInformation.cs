c78d44a8f7bd44c287866a18e088cec2bb59e322
qinsfu <qinsfu@qq.com>
2023-04-20 15:24:05 +0800

    <SmPlatform.Model> 添加视图模型
    1. ChannelInformation
    2. EntityBasicInfo

---
+++ src/SmPlatform.Model/ViewModels/ChannelInformation.cs
@@ -0,0 +1,76 @@
+﻿using SmPlatform.Model.DataModels;
+using System.Collections.Specialized;
+
+namespace SmPlatform.Model.ViewModels;
+
+/// <summary>
+/// 频道信息
+/// </summary>
+public class ChannelInformation : EntityBasicInfo
+{
+    /// <summary>
+    /// 名称
+    /// </summary>
+    public string Name { get; set; }
+
+    /// <summary>
+    /// 平台
+    /// </summary>
+    public Platform Platform { get; set; }
+
+    /// <summary>
+    /// 短信模板
+    /// </summary>
+    public List<Template> Templates { get; set; }
+
+    /// <summary>
+    /// 短信列表
+    /// </summary>
+    public List<Signature> Signatures { get; set; }
+
+    /// <summary>
+    /// 域名
+    /// </summary>
+    public string Domain { get; set; }
+
+    /// <summary>
+    /// 密钥
+    /// </summary>
+    public string AccessKeySecret { get; set; }
+
+    /// <summary>
+    /// 密钥 key
+    /// </summary>
+    public string AccessKeyId { get; set; }
+
+    /// <summary>
+    /// 其他配置
+    /// </summary>
+    public NameValueCollection OtherOptions { get; set; }
+
+    /// <summary>
+    /// 是否可用
+    /// </summary>
+    public bool IsActive { get; set; }
+
+    /// <summary>
+    /// 是否开启
+    /// </summary>
+    public bool IsEnabled { get; set; }
+
+    /// <summary>
+    /// 备注
+    /// </summary>
+    public string Remark { get; set; }
+
+    /// <summary>
+    /// 级别
+    /// </summary>
+    public int Level { get; set; }
+
+    /// <summary>
+    /// 消息内容类型
+    /// </summary>
+    public MessageValueType MessageValueType { get; set; }
+
+}
---
+++ src/SmPlatform.Model/ViewModels/EntityBasicInfo.cs
@@ -0,0 +1,22 @@
+﻿namespace SmPlatform.Model.ViewModels;
+
+/// <summary>
+/// 实体基础信息
+/// </summary>
+public class EntityBasicInfo
+{
+    /// <summary>
+    /// 主键
+    /// </summary>
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// 创建时间
+    /// </summary>
+    public DateTime CreateTime { get; set; }
+
+    /// <summary>
+    /// 更新时间
+    /// </summary>
+    public DateTime UpdateTime { get; set; }
+}
