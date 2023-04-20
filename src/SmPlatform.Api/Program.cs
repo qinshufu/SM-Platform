87901c2da457e5db64b3defdbfcbeed73bd9ea9a
qinsfu <qinsfu@qq.com>
2023-04-20 15:25:08 +0800

    <SmPlatform.Api> 安装并配置 Autofac, AutoMapper, MassTransit

--- src/SmPlatform.Api/Program.cs
+++ src/SmPlatform.Api/Program.cs
@@ -1,4 +1,9 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using MassTransit;
+using MassTransit.Configuration;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using SmPlatform.Api.Instructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +17,34 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<SmsDbContext>(optionsBuilder => optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("default")));
 
+// ÅäÖÃ Autofac 
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory(configurator =>
+{
+    configurator.RegisterAssemblyTypes(typeof(Program).Assembly).AsImplementedInterfaces().InstancePerLifetimeScope();
+
+    //configurator.RegisterAssemblyModules(typeof(Program).Assembly);
+}));
+
+// ÅäÖÃ AutoMapper
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
+
+// ÅäÖÃ MassTransit
+builder.Services.AddOptions<RabbitMqHostSettings>().BindConfiguration(nameof(RabbitMqHostSettings));
+
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.UsingRabbitMq((ctx, conf) =>
+    {
+        conf.Host(ctx.GetRequiredService<IOptions<RabbitMqHostSettings>>().Value);
+    });
+
+    configurator.AddMediator(null);
+});
+
 var app = builder.Build();
+
+// TODO °²×° Autofac 
+// ×¢²á ²Ö´¢·þÎñ
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
