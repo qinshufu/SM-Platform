8bf2dcb11b950c84df8002b31de6717993ed96e1
qinsfu <qinsfu@qq.com>
2023-04-20 18:16:14 +0800

    <SmPlatform.Api.Test> 添加 MassTransit Mediator 的测试

---
+++ test/SmPlatform.Api.Test/MassTransitTest.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using MassTransit;
+using MassTransit.Mediator;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using SmPlatform.Api.Application.Commands;
+using SmPlatform.Api.Domain;
+using SmPlatform.Model.DataModels;
+using SmPlatform.Model.ViewModels;
+
+namespace SmPlatform.Api.Test;
+
+/// <summary>
+/// MassTransit 的接口测试
+/// </summary>
+public class MassTransitTest
+{
+    /// <summary>
+    /// 测试 MassTransit 中介者消息的发送
+    /// </summary>
+    /// <returns></returns>
+    [Fact]
+    public async Task MediatorSendTest()
+    {
+        var command = new ChannelAddCommand();
+        var channel = new Channel();
+        var channelInfo = new ChannelInformation();
+
+        var repositoryMock = new Mock<IChannelRepository>();
+        var mapperMock = new Mock<IMapper>();
+
+        mapperMock.Setup(m => m.Map<Channel>(command)).Returns(channel);
+        mapperMock.Setup(m => m.Map<ChannelInformation>(channel)).Returns(channelInfo);
+
+        repositoryMock.Setup(m => m.AddAsync(channel)).Returns(Task.FromResult(channel));
+
+        var services = new ServiceCollection()
+            .AddScoped(ctx => repositoryMock.Object)
+            .AddScoped(_ => mapperMock.Object)
+            .AddMassTransit(conf => conf.AddMediator(c =>
+            {
+                c.AddConsumer<ChannelAddHandler>();
+            }));
+
+        var result = await services.BuildServiceProvider().GetRequiredService<IScopedMediator>().SendRequest(command);
+
+        Assert.Equal(channel.Id, result.Data!.Id);
+    }
+
+}
