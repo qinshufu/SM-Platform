a9cc7fd230184f8053d6a4f7781f97ec0435d5e9
qinsfu <qinsfu@qq.com>
2023-04-24 00:09:45 +0800

    <SmPlatform.Domain, SmPlatform.Instructure> 添加分布式锁，添加短信发送日志仓库

---
+++ src/SmPlatform.Instructure/Distribution/IDistributedLock.cs
@@ -0,0 +1,24 @@
+﻿namespace SmPlatform.Instructure.Distribution;
+
+/// <summary>
+/// 分布式锁
+/// </summary>
+public interface IDistributedLock
+{
+    /// <summary>
+    /// 标识锁
+    /// </summary>
+    string Key { get; }
+
+    /// <summary>
+    /// 标识谁上的锁
+    /// </summary>
+    string Value { get; }
+}
+
+public record struct DistributedLock : IDistributedLock
+{
+    public string Key { get; init; }
+
+    public string Value { get; init; }
+}
---
+++ src/SmPlatform.Instructure/Distribution/RedisLock.cs
@@ -0,0 +1,17 @@
+﻿namespace SmPlatform.Instructure.Distribution;
+
+/// <summary>
+/// Redis 锁
+/// </summary>
+public record struct RedisLock : IDistributedLock
+{
+    public RedisLock(string key, string value)
+    {
+        Key = key;
+        Value = value;
+    }
+
+    public string Key { get; init; }
+
+    public string Value { get; init; }
+}
